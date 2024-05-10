04f9e423dd24e3bb8985285d9cf4e9cfc44eea03
Rune Soerensen <rsoerensen@salesforce.com>
2024-05-10 10:23:48 -0400

    Add DotnetBuildpack stub

--- buildpacks/dotnet/src/main.rs
+++ buildpacks/dotnet/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, .NET world!");
+use libcnb::build::BuildResultBuilder;
+use libcnb::detect::DetectResultBuilder;
+use libcnb::generic::{GenericMetadata, GenericPlatform};
+use libcnb::{buildpack_main, Buildpack};
+
+buildpack_main! { DotnetBuildpack }
+
+struct DotnetBuildpack;
+
+#[derive(thiserror::Error, Debug)]
+enum DotnetBuildpackError {}
+
+impl Buildpack for DotnetBuildpack {
+    type Platform = GenericPlatform;
+
+    type Metadata = GenericMetadata;
+
+    type Error = DotnetBuildpackError;
+
+    fn detect(
+        &self,
+        _context: libcnb::detect::DetectContext<Self>,
+    ) -> libcnb::Result<libcnb::detect::DetectResult, Self::Error> {
+        DetectResultBuilder::pass().build()
+    }
+
+    fn build(
+        &self,
+        _context: libcnb::build::BuildContext<Self>,
+    ) -> libcnb::Result<libcnb::build::BuildResult, Self::Error> {
+        println!("Hello, World!");
+        BuildResultBuilder::new().build()
+    }
 }
