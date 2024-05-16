aaef63557f54c07966b63a9db15fcd27f7be5173
Rune Soerensen <rsoerensen@salesforce.com>
2024-05-16 07:52:47 -0400

    Use compilation flags for test stubs

--- buildpacks/dotnet/tests/sdk_installation_test.rs
+++ buildpacks/dotnet/tests/sdk_installation_test.rs
@@ -2,10 +2,34 @@ use crate::tests::default_build_config;
 use indoc::formatdoc;
 use libcnb_test::{assert_contains, assert_empty, TestRunner};
 
-// This is just a test stub to test current behavior (of the buildpack itself, and running integration tests locally/on GitHub).
-// TODO: Update when logic to determine SDK version is implemented, allow for multiple archs.
+// These are simply a couple of test drafts (to test current behavior of the buildpack itself,
+// as well as running integration tests locally/on GitHub).
+// TODO: Update when logic to determine SDK version is implemented, and allow for multiple archs.
 #[test]
 #[ignore = "integration test"]
+#[cfg(target_arch = "x86_64")]
+fn sdk_installation_test() {
+    TestRunner::default().build(
+        default_build_config( "tests/fixtures/basic_web_8.0"),
+        |context| {
+            assert_empty!(context.pack_stderr);
+            assert_contains!(
+                context.pack_stdout,
+                &formatdoc! {"
+                    [Determining .NET SDK version]
+                    Using .NET SDK version 8.0.300 (linux-amd64)
+                    Downloading .NET SDK version 8.0.300 from https://download.visualstudio.microsoft.com/download/pr/4a252cd9-d7b7-41bf-a7f0-b2b10b45c068/1aff08f401d0e3980ac29ccba44efb29/dotnet-sdk-8.0.300-linux-x64.tar.gz
+                    Verifying checksum
+                    Installing .NET SDK
+                "}
+            );
+        },
+    );
+}
+
+#[test]
+#[ignore = "integration test"]
+#[cfg(target_arch = "aarch64")]
 fn sdk_installation_test() {
     TestRunner::default().build(
         default_build_config( "tests/fixtures/basic_web_8.0"),
