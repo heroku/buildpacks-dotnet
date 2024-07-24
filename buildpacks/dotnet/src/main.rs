f0bd0b3d5ea3b4a0a2733dcb578e5740ff43ff60
Rune Soerensen <rsoerensen@salesforce.com>
2024-07-24 19:36:44 -0400

    Rewrite process detection warning (#93)

--- buildpacks/dotnet/src/main.rs
+++ buildpacks/dotnet/src/main.rs
@@ -241,13 +241,18 @@ fn log_launch_process_detection_warning(error: &LaunchProcessDetectionError) {
                 formatdoc! {"
                     An invalid launch process type was detected.
 
-                    This buildpack will automatically try to register compiled project executables after successfully publishing an application/solution.
-                    The process type name is based on the name of the executable filename (usually the project name), which in some cases may be
-                    incompatible with the CNB spec; process types can only contain numbers, letters, and the characters `.`, `_`, and `-`.
+                    The buildpack will automatically try to register CNB process types for console and web projects
+                    after successfully publishing an application/solution.
 
-                    Use the warning details below to troubleshoot and make necessary adjustments if you wish to use this automatic registration feature.
-                    
-                    If you believe you've found a bug, or have feedback on how the current behavior could be improved to better fit your use case, file an issue here:
+                    The process type name is based on the name of the executable filename (usually the project name,
+                    e.g. `webapi` or `backgroundworker`), which in some cases may be incompatible with the CNB spec;
+                    process type names can only contain numbers, letters, and the characters `.`, `_`, and `-`.
+
+                    Use the warning details below to troubleshoot and make necessary adjustments if you wish to use
+                    this automatic registration feature.
+
+                    If you believe you've found a bug, or have feedback on how the current behavior could be improved
+                    to better fit your use case, file an issue here:
                     https://github.com/heroku/buildpacks-dotnet/issues/new
 
                     Details: {process_type_error}
