4949f2488be2f55e016efd765514be7ea3622afd
Rune Soerensen <rsoerensen@salesforce.com>
2024-07-25 03:56:52 -0400

    Fix dotnet publish integration test

--- buildpacks/dotnet/tests/sdk_installation_test.rs
+++ buildpacks/dotnet/tests/sdk_installation_test.rs
@@ -58,7 +58,7 @@ fn test_sdk_installation_with_global_json() {
                       - Detected version requirement: `=8.0.101`
                       - Resolved .NET SDK version `8.0.101` (linux-amd64)
                     - SDK installation
-                      - Downloading .NET SDK from https://download.visualstudio.microsoft.com/download/pr/9454f7dc-b98e-4a64-a96d-4eb08c7b6e66/da76f9c6bc4276332b587b771243ae34/dotnet-sdk-8.0.101-linux-x64.tar.gz"
+                      - Downloading SDK from https://download.visualstudio.microsoft.com/download/pr/9454f7dc-b98e-4a64-a96d-4eb08c7b6e66/da76f9c6bc4276332b587b771243ae34/dotnet-sdk-8.0.101-linux-x64.tar.gz"
                 }
             );
             assert_contains!(
