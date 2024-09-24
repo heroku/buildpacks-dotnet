93beb986736f086657837098464726964a9d77e4
Rune Soerensen <rsoerensen@salesforce.com>
2024-09-24 19:56:58 -0400

    Use Inventory FromStr implementation

--- shared/inventory-updater/src/main.rs
+++ shared/inventory-updater/src/main.rs
@@ -20,16 +20,16 @@ fn main() {
         (args[1].clone(), args[2].clone())
     };
 
-    let local_inventory: Inventory<Version, Sha512, Option<()>> = {
-        let content = fs::read_to_string(&inventory_path).unwrap_or_else(|e| {
+    let local_inventory = fs::read_to_string(&inventory_path)
+        .unwrap_or_else(|e| {
             eprintln!("Error reading inventory file at '{inventory_path}': {e}");
             process::exit(1);
-        });
-        toml::from_str(&content).unwrap_or_else(|e| {
+        })
+        .parse::<Inventory<Version, Sha512, Option<()>>>()
+        .unwrap_or_else(|e| {
             eprintln!("Error parsing inventory file at '{inventory_path}': {e}");
             process::exit(1);
-        })
-    };
+        });
 
     let remote_inventory = Inventory {
         artifacts: list_upstream_artifacts(),
