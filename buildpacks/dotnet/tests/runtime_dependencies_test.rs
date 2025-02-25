beca3e381b1c36fc7fb01f434f6d191bb7328a09
Rune Soerensen <rsoerensen@salesforce.com>
2025-02-25 09:18:16 -0500

    Verify runtime libraries with ldd check

--- buildpacks/dotnet/tests/runtime_dependencies_test.rs
+++ buildpacks/dotnet/tests/runtime_dependencies_test.rs
@@ -11,57 +11,11 @@ fn verify_installed_dotnet_runtime_dependencies() {
         let command_output = context.run_shell_command(
             indoc! {r#"
                 set -euo pipefail
-
-                echo "Checking required .NET dependencies..."
-
-                OS_VERSION=$(lsb_release -rs)
-
-                # Define common dependencies (for both Ubuntu 22.04 and 24.04)
-                COMMON_DEPENDENCIES=(
-                    "ca-certificates"
-                    "libc6"
-                    "libgcc-s1"
-                    "libstdc++6"
-                    "libunwind8"
-                    "zlib1g"
-                )
-
-                # Define version-specific dependencies
-                if [[ "$OS_VERSION" == "22.04" ]]; then
-                    VERSION_SPECIFIC_DEPENDENCIES=(
-                        "libgssapi-krb5-2"
-                        "libicu70"
-                        "liblttng-ust1"
-                        "libssl3"
-                    )
-                elif [[ "$OS_VERSION" == "24.04" ]]; then
-                    VERSION_SPECIFIC_DEPENDENCIES=(
-                        "libicu74"
-                        "liblttng-ust1t64"
-                        "libssl3t64"
-                    )
-                else
-                    echo "Unsupported Ubuntu version: ${OS_VERSION}. This script is designed for Ubuntu 22.04 and 24.04."
-                    exit 1
-                fi
-
-                DEPENDENCIES=("${COMMON_DEPENDENCIES[@]}" "${VERSION_SPECIFIC_DEPENDENCIES[@]}")
-                MISSING_PACKAGES=()
-
-                for PACKAGE in "${DEPENDENCIES[@]}"; do
-                    if ! dpkg -s "$PACKAGE" &> /dev/null; then
-                        MISSING_PACKAGES+=("$PACKAGE")
-                    fi
-                done
-
-                if [ ${#MISSING_PACKAGES[@]} -eq 0 ]; then
-                    echo "All required .NET dependencies are installed."
-                    exit 0
-                else
-                    echo "Missing dependencies:"
-                    for PACKAGE in "${MISSING_PACKAGES[@]}"; do
-                        echo "   - $PACKAGE"
-                    done
+                
+                # Check all required dynamically linked libraries can be found in the run image.
+                ldd_output=$(find /layers -type f,l \( -name 'dotnet' -o -name '*.so*' \) -exec ldd '{}' +)
+                if grep 'not found' <<<"${ldd_output}" | sort --unique; then
+                    echo "The above dynamically linked libraries were not found!"
                     exit 1
                 fi
             "#}
