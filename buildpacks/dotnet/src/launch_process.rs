720c9f57e6938622e641a57dbdae5e8ae320df22
Rune Soerensen <rsoerensen@salesforce.com>
2025-03-19 03:03:55 -0400

    Quote launch process command paths

--- buildpacks/dotnet/src/launch_process.rs
+++ buildpacks/dotnet/src/launch_process.rs
@@ -25,14 +25,18 @@ fn project_launch_process(solution: &Solution, project: &Project) -> Option<Proc
 
     let mut command = format!(
         "cd {}; ./{}",
-        relative_executable_path
-            .parent()
-            .expect("Path to always have a parent directory")
-            .display(),
-        relative_executable_path
-            .file_name()
-            .expect("Path to never terminate in `..`")
-            .to_string_lossy()
+        shell_words::quote(
+            &relative_executable_path
+                .parent()
+                .expect("Path to always have a parent directory")
+                .to_string_lossy()
+        ),
+        shell_words::quote(
+            &relative_executable_path
+                .file_name()
+                .expect("Path to never terminate in `..`")
+                .to_string_lossy()
+        )
     );
 
     if project.project_type == ProjectType::WebApplication {
