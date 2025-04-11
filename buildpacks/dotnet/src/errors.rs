277f199b0ba68939a9c6c3d6fc2b7f4498adbbdd
Rune Soerensen <rsoerensen@salesforce.com>
2025-04-11 07:09:28 -0400

    Verify SDK archive file open error output

--- buildpacks/dotnet/src/errors.rs
+++ buildpacks/dotnet/src/errors.rs
@@ -611,6 +611,13 @@ mod tests {
         ));
     }
 
+    #[test]
+    fn test_sdk_layer_open_archive_error() {
+        assert_error_snapshot(&DotnetBuildpackError::SdkLayer(SdkLayerError::OpenArchive(
+            create_io_error(),
+        )));
+    }
+
     fn assert_error_snapshot(error: &DotnetBuildpackError) {
         assert_writer_snapshot(|writer| on_buildpack_error_with_writer(error, writer));
     }
