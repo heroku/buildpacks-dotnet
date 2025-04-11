b899e9b685293d5af29d62dea0d07c300084ce9c
Rune Soerensen <rsoerensen@salesforce.com>
2025-04-11 05:55:35 -0400

    Verify solution file read error output

--- buildpacks/dotnet/src/errors.rs
+++ buildpacks/dotnet/src/errors.rs
@@ -463,6 +463,13 @@ mod tests {
     }
 
     #[test]
+    fn test_load_solution_file_read_error() {
+        assert_error_snapshot(&DotnetBuildpackError::LoadSolutionFile(
+            solution::LoadError::ReadSolutionFile(create_io_error()),
+        ));
+    }
+
+    #[test]
     fn test_parse_global_json_error() {
         assert_error_snapshot(&DotnetBuildpackError::ParseGlobalJson(
             serde::de::Error::custom("foo"),
