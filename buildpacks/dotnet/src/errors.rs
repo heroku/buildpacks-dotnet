05692dfedc390e291d352b19623fba89a4c13c1f
Rune Soerensen <rsoerensen@salesforce.com>
2025-04-11 07:04:23 -0400

    Verify SDK archive extraction error output

--- buildpacks/dotnet/src/errors.rs
+++ buildpacks/dotnet/src/errors.rs
@@ -594,6 +594,13 @@ mod tests {
         ));
     }
 
+    #[test]
+    fn test_sdk_layer_decompress_archive_error() {
+        assert_error_snapshot(&DotnetBuildpackError::SdkLayer(
+            SdkLayerError::DecompressArchive(create_io_error()),
+        ));
+    }
+
     fn assert_error_snapshot(error: &DotnetBuildpackError) {
         assert_writer_snapshot(|writer| on_buildpack_error_with_writer(error, writer));
     }
