1f9510ad228410551fe376f5cf73d8999dc73fd0
Rune Soerensen <rsoerensen@salesforce.com>
2025-05-09 02:58:49 -0400

    Add exception for missing liblttng-ust.so.0

--- buildpacks/dotnet/tests/runtime_dependencies_test.rs
+++ buildpacks/dotnet/tests/runtime_dependencies_test.rs
@@ -13,8 +13,15 @@ fn verify_installed_dotnet_runtime_dependencies() {
                 set -euo pipefail
                 
                 # Check all required dynamically linked libraries can be found in the run image.
+
+                # Capture ldd output for `dotnet` executables and `*.so*` shared libraries in `/layers`.
                 ldd_output=$(find /layers -type f,l \( -name 'dotnet' -o -name '*.so*' \) -exec ldd '{}' +)
-                if grep 'not found' <<<"${ldd_output}" | sort --unique; then
+
+                # Check for missing libraries.
+                # An exception is made for `liblttng-ust.so.0`, which is filtered out as it is considered
+                # non-critical, and expected to be missing in some environments.
+                # For more info, see: https://github.com/heroku/base-images/pull/346#issuecomment-2715075259
+                if grep 'not found' <<<"${ldd_output}" | grep -v 'liblttng-ust.so.0' | sort --unique; then
                     echo "The above dynamically linked libraries were not found!"
                     exit 1
                 else
