671fd870df2aeaaf016f74ad1a62094cecb1bd7d
Rune Soerensen <rsoerensen@salesforce.com>
2025-11-13 02:31:43 -0500

    Implement `From<io::Error> for DiscoveryError`
    Avoid manual mapping for each detect function

--- buildpacks/dotnet/src/app_source.rs
+++ buildpacks/dotnet/src/app_source.rs
@@ -14,6 +14,12 @@ pub(crate) enum DiscoveryError {
     NoAppFound,
 }
 
+impl From<io::Error> for DiscoveryError {
+    fn from(error: io::Error) -> Self {
+        DiscoveryError::DetectionIoError(error)
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub(crate) enum AppSource {
     Solution(PathBuf),
@@ -23,23 +29,20 @@ pub(crate) enum AppSource {
 
 impl AppSource {
     pub(crate) fn from_dir(dir_path: &Path) -> Result<Self, DiscoveryError> {
-        if let Some(path) = detect::solution_file(dir_path)
-            .map_err(DiscoveryError::DetectionIoError)?
-            .map_err(DiscoveryError::MultipleSolutionFiles)?
+        if let Some(path) =
+            detect::solution_file(dir_path)?.map_err(DiscoveryError::MultipleSolutionFiles)?
         {
             return Self::from_file(&path);
         }
 
-        if let Some(path) = detect::project_file(dir_path)
-            .map_err(DiscoveryError::DetectionIoError)?
-            .map_err(DiscoveryError::MultipleProjectFiles)?
+        if let Some(path) =
+            detect::project_file(dir_path)?.map_err(DiscoveryError::MultipleProjectFiles)?
         {
             return Self::from_file(&path);
         }
 
-        if let Some(path) = detect::file_based_app(dir_path)
-            .map_err(DiscoveryError::DetectionIoError)?
-            .map_err(DiscoveryError::MultipleFileBasedApps)?
+        if let Some(path) =
+            detect::file_based_app(dir_path)?.map_err(DiscoveryError::MultipleFileBasedApps)?
         {
             return Self::from_file(&path);
         }
