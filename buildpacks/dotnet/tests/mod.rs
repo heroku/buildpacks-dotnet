a6a530a1228c019f140dcbcba7372794fb995a1a
Rune Soerensen <rsoerensen@salesforce.com>
2026-03-20 18:44:34 -0400

    Set default builder to `heroku/builder:26`
    While the builder isn't GA yet, this will only affect local development as all supported builders will be tested in CI - and is unlikely to cause any issues in either environments

--- buildpacks/dotnet/tests/mod.rs
+++ buildpacks/dotnet/tests/mod.rs
@@ -10,8 +10,7 @@ mod runtime_dependencies_test;
 mod sdk_installation_test;
 mod test_execution_environment_test;
 
-// TODO: Update to `heroku/builder:26` after GA
-const DEFAULT_BUILDER: &str = "heroku/builder:24";
+const DEFAULT_BUILDER: &str = "heroku/builder:26";
 
 pub(crate) fn default_build_config(fixture_path: impl AsRef<Path>) -> BuildConfig {
     let builder = builder();
